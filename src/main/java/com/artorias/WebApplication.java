ff4faf077d6685d7227c0be9f0c3d6312e34c74e
Devin Austin <devin.austin@gmail.com>
2016-10-15 17:20:09 -0600

    removed swagger stuff

--- src/main/java/com/artorias/WebApplication.java
+++ src/main/java/com/artorias/WebApplication.java
@@ -18,34 +18,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.schema.WildcardType;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.time.LocalDate;
 
-@EnableSwagger2
 @SpringBootApplication
 @ComponentScan("com.artorias.controllers")
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.artorias.controllers"))
-                .paths(PathSelectors.ant("/api/*"))
-                .build();
     }
 
 }
--- src/main/java/com/artorias/controllers/PostController.java
+++ src/main/java/com/artorias/controllers/PostController.java
@@ -1,9 +1,5 @@
 package com.artorias.controllers;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +12,11 @@ import java.util.Map;
  * Created by devin on 10/14/16.
  */
 
-@Api(basePath = "/api/posts", value = "Posts", description = "Blog post operations", produces = "application/json")
 @RestController
-@RequestMapping(method = RequestMethod.GET, path="/api/posts", produces= MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(method = RequestMethod.GET, path = "/api/posts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PostController {
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "List posts", notes = "Lists posts")
-    @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Fields are with validation errors"),
-            @ApiResponse(code = 201, message = "") })
-    @RequestMapping(method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Map list() {
         Map hello = new HashMap();
         hello.put("success", true);
